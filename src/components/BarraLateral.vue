1399fea428378eee50eef914b0e4b171df584ef9
dyego.almeida <dyego.almeida@tradetechnology.com.br>
2023-07-27 15:59:18 -0300

    First

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,27 @@
+<template>
+  <header>
+    <h1>
+      <img src="" alt="">
+    </h1>
+  </header>
+
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+header {
+  padding: 1rem;
+  background: #0d3b66;
+  width: 100%;
+  height: 100vh;
+}
+@media only screen and (max-width: 768px) {
+  header {
+    padding: 2.5rem;
+    height: auto;
+  }
+}
+</style>
