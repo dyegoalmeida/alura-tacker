b9cbc7cc62acab4b4ff0d6759bf3a46dbcfd125f
Dyego <dyegocm@gmail.com>
2023-07-30 14:21:46 -0300

    Update project.

---
+++ src/components/ButtonTime.vue
@@ -0,0 +1,35 @@
+<template>
+  <button class="button" @click="click" :disabled="disable">
+    <span class="icon">
+      <i :class="icon"></i>
+    </span>
+    <span>{{text}}</span>
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ButtonTime",
+  emits: ['clicked'],
+  props: {
+    disable: {
+      type: Boolean
+    },
+    icon: {
+      type: String,
+      required: true
+    },
+    text: {
+      type: String,
+      required: true
+    },
+  },
+  methods: {
+    click () : void {
+      this.$emit('clicked')
+    }
+  }
+});
+</script>
--- src/components/Timer.vue
+++ src/components/Timer.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="is-flex is-align-items-center is-justify-content-space-between">
     <StopWatch :time-in-seconds="timeInSeconds"/>
-    <button class="button" @click="start" :disabled="stopwatchStart">
-          <span class="icon">
-            <i class="fas fa-play"></i>
-          </span>
-      <span>play</span>
-    </button>
-    <button class="button" @click="finish" :disabled="!stopwatchStart">
-          <span class="icon">
-            <i class="fas fa-stop"></i>
-          </span>
-      <span>stop</span>
-    </button>
+<!--    <button class="button" @click="start" :disabled="stopwatchStart">-->
+<!--          <span class="icon">-->
+<!--            <i class="fas fa-play"></i>-->
+<!--          </span>-->
+<!--      <span>play</span>-->
+<!--    </button>-->
+<!--    <button class="button" @click="finish" :disabled="!stopwatchStart">-->
+<!--          <span class="icon">-->
+<!--            <i class="fas fa-stop"></i>-->
+<!--          </span>-->
+<!--      <span>stop</span>-->
+<!--    </button>-->
+    <ButtonTime @clicked="start" icon="fas fa-play" text="play" :disable="stopwatchStart" />
+    <ButtonTime @clicked="finish" icon="fas fa-stop" text="stop" :disable="!stopwatchStart" />
   </div>
 </template>
 <script lang="ts">
 import {defineComponent} from "vue";
 import StopWatch from "@/components/StopWatch.vue"
+import ButtonTime from "@/components/ButtonTime.vue";
 
 export default defineComponent({
   name: 'TimerTimer',
   emits: [
       'inTimerFinished'
   ],
-  components: {StopWatch},
+  components: {ButtonTime, StopWatch},
   data () {
     return{
       timeInSeconds: 0,
